cc5c3619bbc9f38956f729c73478e1d50ac61a6a
Robert Fotino <robert.fotino@gmail.com>
2020-05-22 08:47:12 -0700

    Adds basic client and server to start games.
    Right now it will add the same player as both black and white, so
    that I could test out UI functionality. Next update will feature a
    link you can share with another person so they can be the opposite color.

---
+++ app.js
@@ -0,0 +1,188 @@
+const http = require('http');
+const crypto = require("crypto");
+const fs = require('fs');
+
+const chess = require('./lib/chess.js')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let playerIds = new Set();
+let games = new Map();
+
+const STATIC_FILES = {
+  '/': {
+    file: 'client/index.html',
+    contentType: 'text/html',
+  },
+  '/game': {
+    file: 'client/game.html',
+    contentType: 'text/html',
+  },
+  '/index.js': {
+    file: 'client/index.js',
+    contentType: 'text/javascript',
+  },
+  '/styles.css': {
+    file: 'client/styles.css',
+    contentType: 'text/css',
+  },
+};
+
+const API_POST_METHODS = {
+  '/api/create-player-id': function(getParams, postParams, res) {
+    let playerId = '';
+    while (true) {
+      playerId = crypto.randomBytes(8).toString('hex');
+      if (!playerIds.has(playerId)) {
+	playerIds.add(playerId);
+	break;
+      }
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(playerId);
+  },
+  '/api/create-game': function(getParams, postParams, res) {
+    let gameId = '';
+    while (true) {
+      gameId = crypto.randomBytes(8).toString('hex');
+      if (!games.has(gameId)) {
+	games.set(gameId, new chess.ChessGame(gameId));
+	break;
+      }
+    }
+    let playerId = null;
+    if (postParams.hasOwnProperty('playerId') &&
+	postParams.hasOwnProperty('role')) {
+      playerId = postParams.playerId;
+      //games.get(gameId).addPlayer(playerId, postParams.role);
+      // TODO: right now add player to both roles for testing
+      games.get(gameId).addPlayer(playerId, chess.WHITE);
+      games.get(gameId).addPlayer(playerId, chess.BLACK);
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/json');
+    res.end(JSON.stringify(games.get(gameId).toObj(playerId)));
+  },
+  '/api/add-player': function(getParams, postParams, res) {
+    // TODO: Somehow be able to add a second player, probably
+    // not with an API call. Second player will visit special
+    // URL and may have to have playerId generated
+    res.statusCode = 501;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not implemented');
+  },
+  '/api/execute-move': function(getParams, postParams, res) {
+    if (!getParams.has('gameId') ||
+	!postParams.hasOwnProperty('move') ||
+	!postParams.hasOwnProperty('playerId')) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Must have game id, player id, and move info.');
+    } else if (!games.has(getParams.get('gameId'))) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Game not found.');
+    } else {
+      const gameId = getParams.get('gameId');
+      const game = games.get(gameId);
+      const move = postParams.move;
+      const result = game.executeMove(move);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(JSON.stringify({
+	moveResult: result,
+	gameStatus: game.toObj(postParams.playerId),
+      }));
+    }
+  },
+};
+
+const API_GET_METHODS = {
+  '/api/get-game': function(params, res) {
+    if (params.has('id') && games.has(params.get('id'))) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/json');
+      res.end(JSON.stringify(games.get(params.get('id')).toObj()));
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Game not found.');
+    }
+  },
+};
+
+function parseUrl(url) {
+  let parsed = {
+    path: '/',
+    params: new Map(),
+  };
+  const pieces = url.split('?');
+  if (pieces.length >= 1) {
+    parsed.path = pieces[0];
+  }
+  if (pieces.length >= 2) {
+    const paramPieces = pieces[1].split('&');
+    for (let i = 0; i < paramPieces.length; i++) {
+      const param = paramPieces[i].split('=');
+      parsed.params.set(param[0], param[1]);
+    }
+  }
+  return parsed;
+}
+
+const server = http.createServer((req, res) => {
+  console.log(req.method, req.url);
+  const url = parseUrl(req.url);
+
+  switch (req.method) {
+  case 'GET':
+    if (STATIC_FILES.hasOwnProperty(url.path)) {
+      const fileInfo = STATIC_FILES[url.path];
+      fs.readFile(fileInfo.file, function(err, data) {
+	if (err) {
+	  console.log(err);
+	  res.statusCode = 503;
+	  res.setHeader('Content-Type', 'text/plain');
+	  res.end('Internal server error.');
+	} else {
+	  res.statusCode = 200;
+	  res.setHeader('Content-Type', fileInfo.contentType);
+	  res.end(data);
+	}
+      });
+    } else if (API_GET_METHODS.hasOwnProperty(url.path)) {
+      API_GET_METHODS[url.path](url.params, res);
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not found.');
+    }
+    break;
+  case 'POST':
+    if (API_POST_METHODS.hasOwnProperty(url.path)) {
+      let postData = '';
+      req.on('data', chunk => {
+	postData += chunk;
+      });
+      req.on('end', () => {
+	const postParams = postData === '' ? {} : JSON.parse(postData);
+	API_POST_METHODS[url.path](url.params, postParams, res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not found.');
+    }
+    break;
+  default:
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Method not allowed.');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
---
+++ client/index.js
@@ -0,0 +1,228 @@
+var WHITE = 'W';
+var BLACK = 'B';
+
+// https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
+var ASCII_TO_UNICODE = Object.freeze({
+  WK: '\u2654',
+  WQ: '\u2655',
+  WR: '\u2656',
+  WB: '\u2657',
+  WN: '\u2658',
+  WP: '\u2659',
+  BK: '\u265A',
+  BQ: '\u265B',
+  BR: '\u265C',
+  BB: '\u265D',
+  BN: '\u265E',
+  BP: '\u265F',
+  '  ': ' ',
+});
+
+
+var messageElem = document.getElementById('message');
+
+function getPlayerId(callback) {
+  if (document.cookie === '') {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'api/create-player-id');
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+	if (xhr.status === 200) {
+	  var playerId = xhr.responseText;
+	  document.cookie = 'playerid=' + playerId + '; SameSite=Strict';
+	  callback(playerId);
+	} else {
+	  messageElem.innerHTML = 'Error creating player id, please try again.';
+	  console.log("error", xhr.statusText);
+	}
+      }
+    };
+    xhr.send();
+  } else {
+    callback(document.cookie.split('=')[1]);
+  }
+}
+
+function createGame(playerId, color, callback) {
+  console.log('Creating game for player id ' + playerId + ' as role ' + color);
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'api/create-game');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+	const response = JSON.parse(xhr.responseText);
+	if (response.hasOwnProperty('gameId')) {
+	  callback(response.gameId);
+	} else {
+	  messageElem.innerHTML = 'Error creating game, please try again.';
+	  console.log('No game id in response', response);
+	}
+      } else {
+	messageElem.innerHTML = 'Error creating game, please try again.';
+	console.log('error', xhr.statusText);
+      }
+    }
+  };
+  xhr.send(JSON.stringify({
+    playerId: playerId,
+    role: color,
+  }));
+}
+
+function getGameIdFromURL() {
+  var gameId = null;
+  var url = document.location.toString();
+  var urlPieces = url.split('?');
+  if (urlPieces.length === 2) {
+    var paramPieces = urlPieces[1].split('&');
+    for (let i = 0; i < paramPieces.length; i++) {
+      var morePieces = paramPieces[i].split('=');
+      if (morePieces.length === 2) {
+	if (morePieces[0] === 'id') {
+	  gameId = morePieces[1];
+	  break;
+	}
+      }
+    }
+  }
+  return gameId;
+}
+
+var fromSquare = {rank: -1, file: -1};
+var toSquare = {rank: -1, file: -1};
+var isExecutingMove = false;
+function getSquareElem(rank, file) {
+  return document.getElementById('sq-' + rank + '-' + file);
+}
+function executeMove(fromSquare, toSquare) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'api/execute-move?gameId=' + getGameIdFromURL());
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+	const response = JSON.parse(xhr.responseText);
+	updateBoard(response.gameStatus);
+	if (!response.moveResult.success) {
+	  messageElem.innerHTML = response.moveResult.message;
+	}
+      } else if (xhr.status === 404) {
+	messageElem.innerHTML = 'Game not found on server.';
+      } else {
+	messageElem.innerHTML = 'Unknown error executing move, try again.';
+	console.log('error', xhr.status, xhr.statusText);
+      }
+      getSquareElem(fromSquare.rank, fromSquare.file).style.border = '0';
+      getSquareElem(toSquare.rank, toSquare.file).style.border = '0';
+      fromSquare.rank = fromSquare.file = -1;
+      toSquare.rank = toSquare.file = -1;
+      isExecutingMove = false;
+    }
+  };
+  getPlayerId(function(playerId) {
+    xhr.send(JSON.stringify({
+      playerId,
+      move: {
+	playerId,
+	data: {
+	  // TODO: support pawn promotion and castling in the UI
+	  srcPos: fromSquare,
+	  dstPos: toSquare,
+	  pawnPromotion: 'Q',
+	},
+      },
+    }));
+  });
+}
+function clickSquare(rank, file) {
+  if (isExecutingMove) {
+    return;
+  }
+  if (fromSquare.rank === -1 && fromSquare.file === -1) {
+    fromSquare = {rank, file};
+    getSquareElem(rank, file).style.border = '3px solid black';
+  } else if (fromSquare.rank === rank && fromSquare.file === file) {
+    // Click same square to deselect
+    fromSquare.rank = fromSquare.file = -1;
+    getSquareElem(rank, file).style.border = '0';
+  } else {
+    toSquare = {rank, file};
+    getSquareElem(rank, file).style.border = '3px solid black';
+    isExecutingMove = true;
+    executeMove(fromSquare, toSquare);
+  }
+}
+
+function initBoard() {
+  var boardElem = document.getElementById('board');
+  var lightSquare = true;
+  for (var rank = 0; rank < 8; rank++) {
+    var rankElem = document.createElement('tr');
+    rankElem.className = 'board-row';
+    for (var file = 0; file < 8; file++) {
+      var squareElem = document.createElement('td');
+      squareElem.id = 'sq-' + rank + '-' + file;
+      squareElem.className = 'board-square-' + (lightSquare ? 'light' : 'dark');
+      squareElem.innerHTML = '&nbsp;';
+      squareElem.addEventListener('click', (function(rank, file) {
+	return function() { clickSquare(rank, file); };
+      })(rank, file));
+      rankElem.appendChild(squareElem);
+      lightSquare = !lightSquare;
+    }
+    boardElem.appendChild(rankElem);
+    lightSquare = !lightSquare;
+  }
+}
+
+function updateBoard(response) {
+  // TODO: Should say something like "waiting for other player to join"
+  // or "it's your turn" or "waiting for other player to move".
+  messageElem.innerHTML = 'Waiting for input.';
+  for (var rank = 0; rank < response.board.length; rank++) {
+    for (var file = 0; file < response.board[rank].length; file++) {
+      var squareElem = document.getElementById('sq-' + rank + '-' + file);
+      squareElem.innerHTML = ASCII_TO_UNICODE[response.board[rank][file]];
+    }
+  }
+}
+
+function loadGame() {
+  initBoard();
+  var gameId = getGameIdFromURL();
+  if (gameId === null) {
+    messageElem.innerHTML = 'No game id found in URL.';
+  } else {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'api/get-game?id=' + gameId);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+	if (xhr.status === 200) {
+	  const response = JSON.parse(xhr.responseText);
+	  updateBoard(response);
+	} else if (xhr.status === 404) {
+	  messageElem.innerHTML = 'Game not found on server.';
+	} else {
+	  messageElem.innerHTML = 'Unknown error loading game.';
+	  console.log('error', xhr.status, xhr.statusText);
+	}
+      }
+    };
+    xhr.send();
+  }
+}
+
+function startGameAsWhite() {
+  getPlayerId(function(playerId) {
+    createGame(playerId, WHITE, function(gameId) {
+      window.location.href = 'game?id=' + gameId;
+    });
+  });
+}
+
+function startGameAsBlack() {
+  getPlayerId(function(playerId) {
+    createGame(playerId, BLACK, function(gameId) {
+      window.location.href = 'game?id=' + gameId;
+    });
+  });
+}
